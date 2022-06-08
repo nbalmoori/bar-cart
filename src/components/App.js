9529bfe3dfe6c6a9b968f9390257d1a1196af2e2
Nikhila Balmoori <nbalmoori@gmail.com>
2022-06-08 11:48:18 -0600

    Reorganize file structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Ingredients from './Ingredients.js'
+import '../styling/App.css';
+
+const App = () => {
+  return (
+    <main className="App">
+      <header>BAR CART</header>
+      <Ingredients />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Ingredients.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styling/Ingredients.css';
+
+const Ingredients = () => {
+  return (
+    <div>
+      <p>Ingredients</p>
+    </div>
+  )
+}
+
+export default Ingredients;
