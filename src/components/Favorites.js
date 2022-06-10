3b4fe0a9e443ca8fa0c78aa4293632383ef04889
Nikhila Balmoori <nbalmoori@gmail.com>
2022-06-10 11:56:58 -0600

    Add links to favorites and home page views

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
 import '../styling/Favorites.css';
 
 const Favorites = () => {
   return (
-    <div>favorites!</div>
+    <div>
+      <Link to={`/`}><button>Home</button></Link>
+    </div>
   );
 };
 
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import IngredientsContainer from './IngredientsContainer';
 import FilteredCocktailContainer from './FilteredCocktailContainer';
 
 const HomePage = ({ ingredientsList, selectFilter, filter, filteredRecipes}) => {
   return (
     <div>
+      <Link to={`/favorites`}><button>See Your Favorites</button></Link>
       <IngredientsContainer ingredientsList={ingredientsList} selectFilter={selectFilter}/>
       <FilteredCocktailContainer filter={filter} filteredRecipes={filteredRecipes}/>
     </div>
