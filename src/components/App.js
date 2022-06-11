1751b4ae8c1b76a1d04df3389990a1e5419365c8
Nikhila Balmoori <nbalmoori@gmail.com>
2022-06-11 10:39:50 -0600

    Create and style homepage header

--- src/components/App.js
+++ src/components/App.js
@@ -39,8 +39,6 @@ class App extends Component {
   render() {
     return (
       <main className="App">
-        <header>BAR CART</header>
-        <img src="./homepage.jpg"/>
         <Switch>
           <Route exact path='/' render={() => <Homepage ingredientsList={this.state.ingredientsList} selectFilter={this.selectFilter} filter={this.state.filter} filteredCocktails={this.state.filteredCocktails}/>}/> 
           <Route exact path='/favorites' render={() => <Favorites favorites={this.state.favorites}/>} />
